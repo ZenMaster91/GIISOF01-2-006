bad4bc95b5204b616916587ec35043ce76528c29
Guillermo Facundo Colunga <colunga91@gmail.com>
2018-03-07 19:57:30 +0100

    update

--- V0/Processor.c
+++ V0/Processor.c
@@ -78,7 +78,7 @@ void Processor_DecodeAndExecuteInstruction() {
   // Instruction MEMADD
   case 'm':
     Buses_write_AddressBus_From_To(CPU, MMU);
-    MainMemmory_readMemory();
+    MainMemory_readMemory();
     registerAccumulator_CPU =
         registerIR_CPU.operand1 + registerMBR_CPU.operand1;
     registerPC_CPU++;
